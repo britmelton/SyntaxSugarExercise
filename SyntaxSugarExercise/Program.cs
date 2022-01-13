cb18566dc6c7d6eaf94d815b787c8be29d58c989
brittany melton <brittanymelton26@gmail.com>
2022-01-13 12:40:55 -0500

    syntax sugar exercise

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? answer + " is less than nine" : answer + " greater than or equal to nine";
+           Console.Write(response); 
+
+            var i = 1;
+            var positiveMessage = (i > 0) ? "\nyou are positive" : "\nyour are not positive";
+            Console.WriteLine(positiveMessage);
+
+        }
+    }
+}
