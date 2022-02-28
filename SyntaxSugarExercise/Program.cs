c426e01c2daec1ee399a408e9edd8e266ed89a8e
brittany melton <brittanymelton26@gmail.com>
2022-02-28 16:52:33 -0500

    added class exercise instruction comments

--- SyntaxSugarExercise/Program.cs
+++ SyntaxSugarExercise/Program.cs
@@ -6,10 +6,26 @@ namespace SyntaxSugarExercise
     {
         static void Main(string[] args)
         {
+            #region Class Exercise Instructions
+            //in class exercise 
+            /*Given the following code, change the code to implement inferred typing, string interpolation, and the ternary operator.
+            int answer = 4;
+            string response;
+            if (answer < 9) 
+            {
+                response = answer + " is less than nine";
+            }
+	        else
+	        {
+	            response = answer + “greater than or equal to nine”;
+	        } */
+            #endregion
+
             var answer = 4;
             var response = (answer < 9) ? answer + " is less than nine" : answer + " greater than or equal to nine";
            Console.Write(response); 
 
+            //bonus practice 
             var i = 1;
             var positiveMessage = (i > 0) ? "\nyou are positive" : "\nyour are not positive";
             Console.WriteLine(positiveMessage);
